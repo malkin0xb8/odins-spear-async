ab52db61259374f32724f3704c2a8d2cd6cfa355
Jordan Prescott <Jprescott23@gmail.com>
2025-05-30 09:49:13 +0100

    Mic-users (#237)
    Make it Complete Users endpoint completed

--- src/odins_spear/endpoints/users.py
+++ src/odins_spear/endpoints/users.py
@@ -76,6 +76,22 @@ class Users(BaseEndpoint):
 
         return self._requester.get(endpoint, params=params)
 
+    def get_user_password(self, user_id: str):
+        """Returns login and password expiry details of target user.
+
+        Args:
+            user_id (str):  Target user ID.
+
+        Returns:
+            dict: Details of user
+        """
+
+        endpoint = "/users/password"
+
+        params = {"userId": user_id}
+
+        return self._requester.get(endpoint, params=params)
+
     def get_user_by_id(self, user_id: str):
         """Returns extensive details of a single user including alias, enpoint device, and more common
         details like first and last name.
@@ -90,6 +106,71 @@ class Users(BaseEndpoint):
         endpoint = "/users"
 
         params = {"userId": user_id}
+
+        return self._requester.get(endpoint, params=params)
+
+    def get_user_audit(self, user_id: str):
+        """Returns a detailed audit of user spanning from generic details to call policies and features assigned.
+
+        Args:
+            user_id (str): Target user ID to audit
+
+        Returns:
+            Dict: Audit details of user
+        """
+
+        endpoint = "/users/audit"
+
+        params = {"userId": user_id}
+
+        return self._requester.get(endpoint, params=params)
+
+    def get_user_login_info(self, user_id: str):
+        """Returns detailed log in information such as password expirery
+
+        Args:
+            user_id (str): Target user ID
+
+        Returns:
+            Dict: Login details of target user
+        """
+
+        endpoint = "/users/login-info"
+
+        params = {"userId": user_id}
+
+        return self._requester.get(endpoint, params=params)
+
+    def get_user_portal_passcode(self, user_id: str):
+        """Returns portal passcode details such as expirery
+
+        Args:
+            user_id (str): Target user ID
+
+        Returns:
+            Dict: Portal passcode details of target user
+        """
+
+        endpoint = "/users/portal-passcode"
+
+        params = {"userId": user_id}
+
+        return self._requester.get(endpoint, params=params)
+
+    def get_group_user_audit(self, service_provider_id: str, group_id: str):
+        """AI is creating summary for get_group_user_audit
+
+        Args:
+            service_provider_id (str): Target Service Provider ID
+            group_id (str): Target Group ID with users
+
+        Returns:
+            Dict: Audit of users in specified group
+        """
+
+        endpoint = "/groups/users/audit"
+
+        params = {"serviceProviderId": service_provider_id, "groupId": group_id}
 
         return self._requester.get(endpoint, params=params)
 
@@ -142,6 +223,46 @@ class Users(BaseEndpoint):
         payload["lastName"] = last_name
         payload["extension"] = extension
         payload["password"] = web_auth_password
+
+        return self._requester.post(endpoint, data=payload)
+
+    def post_user_reset(
+        self,
+        user_id: str,
+        remove_from_group_services: bool,
+        remove_call_records: bool,
+        remove_alternate_user_ids: bool,
+        remove_webex_person: bool,
+        cycle_service_packs: bool,
+        reset_password_passcode: bool,
+    ):
+        """Resets/ removes user across areas such as resetting password and cycling feature pack
+
+        Note: Not fully understood, use with caution
+
+        Args:
+            user_id (str): Users Original Identifier
+            remove_from_group_services (bool): Remove From Group Services
+            remove_call_records (bool): Remove Call Record Instances
+            remove_alternate_user_ids (bool): Remove Alternate User Identifiers
+            remove_webex_person (bool): Remove Webex Entry
+            cycle_service_packs (bool): Shift Service Packs
+            reset_password_passcode (bool): Reset Password Forcing A New Login And Password
+        Returns:
+            None: This function doesn't return any details
+        """
+
+        endpoint = "/users/reset"
+
+        payload = {
+            "userId": user_id,
+            "removeFromGroupServices": remove_from_group_services,
+            "removeCallRecords": remove_call_records,
+            "removeAlternateUserIds": remove_alternate_user_ids,
+            "removeWebexPerson": remove_webex_person,
+            "cycleServicePacks": cycle_service_packs,
+            "resetPasswordPasscode": reset_password_passcode,
+        }
 
         return self._requester.post(endpoint, data=payload)
 
@@ -217,4 +338,94 @@ class Users(BaseEndpoint):
 
         return self._requester.put(endpoint, data=data)
 
+    def put_user_sip_contacts(
+        self, service_provider_id: str, group_id: str, user_id: str, sip_contacts: list
+    ):
+        """Adds a list of SIP Contacts to a specified user. If adding only 1 still include in a list.
+
+        Note: 5 is the max amount of sip contats a user can have
+
+        Args:
+            service_provider_id (str): Service Provider ID which hosts Group
+            group_id (str): Group ID which hosts User
+            user_id (str): Target User ID
+            sip_contact (list): List of sip contact details to add. 5 max.
+
+        Returns:
+            None: This function doesn't return any details
+        """
+
+        if sip_contacts.len() > 5:
+            from ..exceptions import OSValueExceeded
+
+            raise OSValueExceeded
+
+        endpoint = "/users"
+
+        data = {
+            "serviceProviderId": service_provider_id,
+            "groupId": group_id,
+            "userId": user_id,
+            "contacts": [{"sipContact": contact} for contact in sip_contacts],
+        }
+
+        return self._requester.put(endpoint, data=data)
+
+    def put_user_id(self, current_user_id: str, new_user_id: str):
+        """Updates a single user ID to a new user ID.
+
+        Args:
+            current_user_id (str): Existing User ID
+            new_user_id (str): User ID to change to
+
+        Returns:
+            None: This function doesn't return any details
+        """
+
+        endpoint = "/users/user-id"
+
+        data = {"userId": current_user_id, "newUserId": new_user_id}
+
+        return self._requester.put(endpoint, data=data)
+
+    def put_group_id_update(self, user_id: str, group_id: str, evaluate: bool = False):
+        """Updates the Group the user belongs to.
+
+        Note: Not fully understood - use with caution
+
+        Args:
+            user_id (str): Target user ID
+            group_id (str): Target grouo ID to migrate to
+            evaluate (bool, optional): True or False. Defaults to False.
+
+        Returns:
+            None
+        """
+
+        endpoint = "/users/group-id"
+
+        data = {
+            "userId": user_id,
+            "newGroupId": group_id,
+            "evaluateOnly": evaluate,
+        }
+
+        return self._requester.put(endpoint, data=data)
+
     # DELETE
+
+    def delete_user(self, user_id: str):
+        """Removes a single user form the platform
+
+        Args:
+            user_id (str): Target user ID
+
+        Returns:
+            None
+        """
+
+        endpoint = "/users"
+
+        params = {"userId": user_id}
+
+        return self._requester.delete(endpoint, params=params)
